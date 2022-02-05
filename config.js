4d0a5d45ce0e63d45842694c0888278b1bde9451
Alifbotz <aluy70003@gmail.com>
2022-02-05 14:19:29 +0700

    script

---
+++ config.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const chalk = import('chalk')
+const setting = JSON.parse(fs.readFileSync('./settings.json'))
+
+// Website Api
+global.APIs = {
+	alfa: 'https://api-alphabot.herokuapp.com',
+	zeks: 'https://zeks.me',
+}
+
+// Free apikey
+global.APIKeys = {
+	'https://api-alphabot.herokuapp.com': 'Alphabot',
+	'https://zeks.me': 'Alphabott',
+}
+
+global.owner = ['6281915605137','918156874290']
+global.packname = setting.botname
+global.author = setting.ownername
+global.alphaVersion = [2, 2143, 3]
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright("Update 'config.js'"))
+	delete require.cache[file]
+	require(file)
+})
---
+++ shop/sewa.js
@@ -0,0 +1,28 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = Rp 5.000
+⬡ 2 MINGGU = Rp 8.000
+⬡ 3 MINGGU = Rp 10.000
+⬡ 1 BULAN = Rp 15.000
+⬡ PERMANEN = Rp 100.000
+⬡ PERMANEN + USER PREM = Rp 150.000
+
+
+⬣ PRICE JADIBOT
+
+⬡ JADI BOT PERMANEN 25K
+
+
+⬣ MINAT? PM
+⬡ wa.me/6281915605137
+
+
+NOTE : 
+Bot On tidak 24 Jam Karna Tidak Menggunakan Heroku [Makanya donasi biar bisa on 24 jam]
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
